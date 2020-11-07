ee472056d548e22f00a145926d1daa252d7788c0
Ahri Earl Spilner <ozogtea07@gmail.com>
2020-11-07 09:27:26 +0700

    Tugas 7 Javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+var tinggi = [165,169,170,171,175,168,167,178,180,177]
+for (var i = 0; i < tinggi.length; i++) {
+  console.log(tinggi[i]);
+}
+
+console.log("");
+
+var tinggi = [165,169,170,171,175,168,167,178,180,177]
+for (let x of tinggi) {
+  console.log(x);
+}
